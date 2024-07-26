57ed4398320fe9027f222bdf34f30752c0a0b485
Max <max@maxf.net>
2024-07-25 20:56:35 -0400

    updated

---
+++ src/main/java/org/apache/kafka/common/requests/HTTPProduceRequestParser.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.common.requests;
+
+import java.nio.ByteBuffer;
+
+import org.apache.kafka.common.protocol.ByteBufferAccessor;
+import org.apache.kafka.common.message.ProduceRequestData;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HTTPProduceRequestParser extends ProduceRequestParser {
+	public static final Logger log = LoggerFactory.getLogger(HTTPProduceRequestParser.class);
+
+	private HttpClient httpClient = HttpClient.newHttpClient();
+
+	public HTTPProduceRequestParser() {
+	}
+
+    public ProduceRequest parse(ByteBuffer buffer, short version) {
+		HttpRequest request = HttpRequest.newBuilder()
+			.uri(URI.create(serviceUrl))
+			.POST(HttpRequest.BodyPublishers.noBody())
+			.build();
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return new ProduceRequest(new ProduceRequestData(new ByteBufferAccessor(buffer), version), version);
+    }
+}
