f7f2dc6d9d5b40bcbb77be6f3a736dd5f96dcc0e
Max <max@maxf.net>
2024-08-12 15:39:19 -0400

    updated

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -44,9 +44,3 @@ testing {
     }
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(11)
-    }
-}
